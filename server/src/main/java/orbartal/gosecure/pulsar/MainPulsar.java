d30d61d626c11295b20ccbe8aa10dc041e7ba3b9
orbartal <orbartal@gmail.com>
2020-09-13 09:23:31 -0400

    Project skeleton

---
+++ server/src/main/java/orbartal/gosecure/pulsar/MainPulsar.java
@@ -0,0 +1,13 @@
+package orbartal.gosecure.pulsar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainPulsar {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainPulsar.class, args);
+    }
+
+}
